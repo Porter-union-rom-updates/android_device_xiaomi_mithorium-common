2848fdcb1b91f4f47efe917d8642e72d1791b921
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2024-11-02 23:30:11 +0200

    mithorium-common: Add lite protobuf library

--- extract-files.sh
+++ extract-files.sh
@@ -95,7 +95,7 @@ function blob_fixup() {
             done
             ;;
         vendor/lib64/libwvhidl.so|vendor/lib64/mediadrm/libwvdrmengine.so)
-            sed -i 's|libprotobuf-cpp-lite-3.9.1.so|libprotobuf-cpp-full-3.9.1.so|g' "${2}"
+            #sed -i 's|libprotobuf-cpp-lite-3.9.1.so|libprotobuf-cpp-full-3.9.1.so|g' "${2}"
             [ "$2" = "" ] && return 0
             grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
